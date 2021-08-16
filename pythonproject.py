507e227e7c21aac1a83926502b72baabb3afa73c
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2021-08-16 14:15:22 -0400

    Update pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,9 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!!</h1>"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    return render_template("index.html")
